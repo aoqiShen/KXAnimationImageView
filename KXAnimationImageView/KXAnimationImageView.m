f4d2a2411a7cbb58279e968343dd6cbe8e604a18
xiaoliang.wang <xiaoliang.wang@qunar.com>
2016-02-24 23:46:51 +0800

    first commit

---
+++ KXAnimationImageView/KXAnimationImageView.m
@@ -0,0 +1,13 @@
+//
+//  KXAnimationImageView.m
+//  KXAnimationImageView
+//
+//  Created by 王小亮 on 16/2/24.
+//  Copyright © 2016年 王小亮. All rights reserved.
+//
+
+#import "KXAnimationImageView.h"
+
+@implementation KXAnimationImageView
+
+@end
